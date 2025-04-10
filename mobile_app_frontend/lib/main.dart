50812380d7616db637e52d795ec948bda4ba1590
kin-lgtm <143951269+kin-lgtm@users.noreply.github.com>
2025-04-10 10:52:05 +0530

    Update main

--- mobile_app_frontend/lib/main.dart
+++ mobile_app_frontend/lib/main.dart
@@ -1,17 +1,1 @@
-import 'package:flutter/material.dart';
-import 'presentation/pages/appointment_page.dart';
-import 'presentation/pages/feedback_page.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FeedbackPage(), // Navigate to the new AppointmentView page
-    );
-  }
-}
