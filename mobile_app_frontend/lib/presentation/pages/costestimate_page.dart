5cb4b14d94bfc3eaf479ba0b1f84ca4cd53152d4
kin-lgtm <143951269+kin-lgtm@users.noreply.github.com>
2025-04-17 01:45:42 +0530

    Update the cost estimate page

--- mobile_app_frontend/lib/presentation/pages/costestimate_page.dart
+++ mobile_app_frontend/lib/presentation/pages/costestimate_page.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:mobile_app_frontend/presentation/components/atoms/cost_estimate_table.dart';
 import 'package:mobile_app_frontend/core/theme/app_colors.dart';
-import 'package:mobile_app_frontend/core/theme/app_text_styles.dart';
+import 'package:mobile_app_frontend/presentation/components/molecules/custom_app_bar.dart';
+import 'package:mobile_app_frontend/presentation/components/atoms/cost_estimate_description.dart';
+import 'package:mobile_app_frontend/presentation/components/atoms/button.dart';
+import 'package:mobile_app_frontend/presentation/components/atoms/enums/button_type.dart';
+import 'package:mobile_app_frontend/presentation/components/atoms/enums/button_size.dart';
 
 class CostEstimatePage extends StatelessWidget {
+  const CostEstimatePage({super.key});
+
+  void _handleBookAppointment() {
+    print('Proceeding to the BookAppointment');
+  }
+
   @override
   Widget build(BuildContext context) {
+    final List<String> services = [
+      "Oil Changing",
+      "Tire Changing",
+      "Engine Check",
+      "Brake Check",
+      "Body Check"
+    ];
+    final List<int> costs = [40000, 30000, 20000, 50000, 25000];
+    final int total = 170000;
+
     return Scaffold(
+      appBar: CustomAppBar(
+        title: 'Cost Estimation',
+        showTitle: true,
+        onBackPressed: () => Navigator.of(context).pop(),
+      ),
       backgroundColor: AppColors.neutral400,
       body: SafeArea(
         child: Padding(
@@ -14,70 +39,33 @@ class CostEstimatePage extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                "Janaka Motors",
-                style: AppTextStyles.body3Bold
-                    .copyWith(color: AppColors.neutral200),
-              ),
-              const SizedBox(height: 16),
-              Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 28, vertical: 16),
-                height: 192,
-                width: 400,
-                decoration: BoxDecoration(
-                  color: Color(0xFF2C2C2E),
-                  borderRadius: BorderRadius.circular(8),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    InfoText(
-                      label: "Vehicle Registration No",
-                      value: "AB3424",
-                    ),
-                    InfoText(label: "Appointment Date", value: "12/03/2025"),
-                    InfoText(label: "Loyalty Points", value: "4524"),
-                    InfoText(label: "Service Center ID", value: "3421"),
-                    InfoText(
-                        label: "Address",
-                        value: "No: 109, Galle Road, Moratuwa"),
-                    InfoText(label: "Distance", value: "25 km away"),
-                  ],
-                ),
+              const CostEstimateDescription(
+                servicecenterName: "Janaka Motors",
+                vehicleRegNo: "AB3424",
+                appointmentDate: "12/03/2025",
+                loyaltyPoints: "4524",
+                serviceCenterId: "3421",
+                address: "No: 109, Galle Road, Moratuwa",
+                distance: "25 km away",
               ),
               const SizedBox(height: 48),
-              Expanded(child: CostEstimateTable()),
+              CostEstimateTable(
+                services: services,
+                costs: costs,
+                total: total,
+              ),
+              const SizedBox(height: 80),
+              SizedBox(
+                width: double.infinity,
+                child: CustomButton(
+                  label: 'Book an Appointment',
+                  type: ButtonType.primary,
+                  size: ButtonSize.medium,
+                  onTap: _handleBookAppointment,
+                ),
+              ),
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class InfoText extends StatelessWidget {
-  final String label;
-  final String value;
-
-  const InfoText({required this.label, required this.value});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 2),
-      child: RichText(
-        text: TextSpan(
-          text: "$label : ",
-          style:
-              AppTextStyles.body3Regular.copyWith(color: AppColors.neutral200),
-          children: [
-            TextSpan(
-              text: value,
-              style: AppTextStyles.body3Regular
-                  .copyWith(color: AppColors.neutral200),
-            ),
-          ],
         ),
       ),
     );
