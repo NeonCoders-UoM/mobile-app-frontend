9e90c81e621e90f7426f2a200ff9968fbfc3c2f1
kin-lgtm <143951269+kin-lgtm@users.noreply.github.com>
2025-04-16 14:46:59 +0530

    update the date picker according to the appointment booking process

--- mobile_app_frontend/lib/presentation/components/atoms/date_picker.dart
+++ mobile_app_frontend/lib/presentation/components/atoms/date_picker.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:mobile_app_frontend/core/theme/app_text_styles.dart';
 import 'package:mobile_app_frontend/core/theme/app_colors.dart';
-import 'package:mobile_app_frontend/presentation/components/atoms/custom_text_field.dart';
+import 'package:mobile_app_frontend/core/theme/app_text_styles.dart';
+import 'package:mobile_app_frontend/presentation/components/atoms/text_field.dart';
+import 'package:mobile_app_frontend/presentation/components/atoms/enums/input_field_state.dart';
 
 class DatePicker extends StatefulWidget {
+  final DateTime? initialDate;
+  final ValueChanged<DateTime>? onDateChanged;
+
+  const DatePicker({
+    super.key,
+    this.initialDate,
+    this.onDateChanged,
+  });
+
   @override
   _DatePickerState createState() => _DatePickerState();
 }
@@ -23,21 +33,36 @@ class _DatePickerState extends State<DatePicker> {
     if (picked != null && picked != selectedDate) {
       setState(() {
         selectedDate = picked;
-        _dateController.text = DateFormat('yyyy-MM-dd').format(selectedDate!);
+        _dateController.text = DateFormat('yyyy-MM-dd').format(picked);
       });
+      if (widget.onDateChanged != null) {
+        widget.onDateChanged!(picked);
+      }
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    selectedDate = widget.initialDate ?? DateTime.now();
+    _dateController.text = DateFormat('yyyy-MM-dd').format(selectedDate!);
+  }
+
+  @override
+  void dispose() {
+    _dateController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(16),
-      padding: EdgeInsets.all(30),
+      padding: const EdgeInsets.all(20),
       width: 360,
       height: 140,
       decoration: BoxDecoration(
         color: AppColors.neutral450,
-        border: Border.all(color: AppColors.neutral200, width: 1.5),
+        border: Border.all(color: AppColors.neutral200, width: 1.0),
         borderRadius: BorderRadius.circular(8),
       ),
       child: Column(
@@ -45,20 +70,25 @@ class _DatePickerState extends State<DatePicker> {
         children: [
           Text(
             'Appointment Date',
-            style: AppTextStyles.heading2Regular
-                .copyWith(color: AppColors.neutral100, fontSize: 20),
+            style: AppTextStyles.displayMdRegular.copyWith(
+              color: AppColors.neutral100,
+              fontSize: 20,
+            ),
           ),
-          SizedBox(height: 8),
+          const SizedBox(height: 8),
           GestureDetector(
             onTap: () => _selectDate(context),
             child: Row(
               children: [
                 Expanded(
-                  child: CustomTextField(),
-                ),
-                IconButton(
-                  icon: Icon(Icons.calendar_today, color: AppColors.neutral150),
-                  onPressed: () => _selectDate(context),
+                  child: InputFieldAtom(
+                    state: InputFieldState.defaultState,
+                    placeholder: 'Select date',
+                    controller: _dateController,
+                    trailingIcon: Icons.calendar_today,
+                    showTrailingIcon: true,
+                    onTrailingIconTap: () => _selectDate(context),
+                  ),
                 ),
               ],
             ),
